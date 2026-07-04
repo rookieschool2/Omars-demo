24ed52907e413ccb9974af5895946fef1a762ce0
Mason Goché <goche.mason@gmail.com>
2026-07-04 00:42:20 -0700

    Redirect old /shop URL to /store

--- omars-demo/next.config.mjs
+++ omars-demo/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/shop',
+        destination: '/store',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
